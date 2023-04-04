743e3b55ddfa6b5bdddace48fcc64005b1d1a3f2
Volamarosoa <121237376+Volamarosoa@users.noreply.github.com>
2023-04-04 14:44:03 +0300

    bon

--- application/helpers/testFonction_helper.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    if ( ! function_exists('ma_fonction')) {
-        function compteVola($vola, $porcentage){
-            echo  $vola . ' ===> ' . (($vola * $porcentage) / 100) . 'Ar';
-        }
-    } 
-?>
